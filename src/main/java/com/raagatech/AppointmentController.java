d122bdccd663dc58de77672c1ab0fe39ada89ccf
sarveshscjp06 <sarvesh.new@gmail.com>
2024-03-02 21:28:57 +0530

    appointment form

--- src/main/java/com/raagatech/AppointmentController.java
+++ src/main/java/com/raagatech/AppointmentController.java
@@ -1,6 +1,10 @@
 package com.raagatech;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppointmentController {
