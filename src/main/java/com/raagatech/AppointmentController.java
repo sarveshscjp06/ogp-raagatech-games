657dc4ae0b618662f72e1453a6b5296960604e6e
sarveshscjp06 <sarvesh.new@gmail.com>
2024-03-09 12:08:59 +0530

    appointment form

--- src/main/java/com/raagatech/AppointmentController.java
+++ src/main/java/com/raagatech/AppointmentController.java
@@ -1,5 +1,10 @@
 package com.raagatech;
 
+import com.raagatech.common.datasource.EmailUtilityInterface;
+import com.raagatech.omp.musicapp.RaagatechMusicDataSource;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +14,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class AppointmentController {
 
+    @Autowired
+    private RaagatechMusicDataSource musicDataSource;
+    @Autowired
+    private EmailUtilityInterface emailUtility;
+
     @GetMapping("/appointment")
-    public String appointmentForm(Model model){
+    public String appointmentForm(Model model) {
 
         model.addAttribute("appointment", new Appointment());
         return "appointment";
     }
 
     @PostMapping("/appointment")
-    public String appointmentSubmit(@ModelAttribute Appointment appointment, Model model){
+    public String appointmentSubmit(@ModelAttribute Appointment appointment, Model model) {
 
+        boolean appointmentStatus = Boolean.FALSE;
+        try {
+            appointmentStatus = musicDataSource.insertInquiry(appointment.getName(), 1, appointment.getEmail(), appointment.getPhone(), 1,
+                    "appointment", appointment.getFollowupDetails(), "091", "", "", "", 0, "",
+                    "", "", "", "", 1);
+        } catch (Exception ex) {
+            Logger.getLogger(AppointmentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (appointmentStatus) {
+            String body = "Hi ".concat(appointment.getName()).concat(", ")
+                    .concat("The appointment details have been sent via SMS and e-mail service.")
+                    .concat("Thanks!");
+            emailUtility.sendEmail(appointment.getEmail(), "raagatech: inquiry registration", body);
+            appointment.setContent(body);
+        }
         model.addAttribute("appointment", appointment);
+
         return "result";
     }
 
